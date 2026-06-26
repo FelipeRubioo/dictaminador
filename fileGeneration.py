91f7f2f7c5a9db51314b41334a36c3f5a6580d5c
FelipeRubioo <feliperubio9201@gmail.com>
2026-06-26 13:22:41 -0700

    fileGeneration-encabezado

--- fileGeneration.py
+++ fileGeneration.py
@@ -1,6 +1,9 @@
 from docx import Document
-from docx.enum.text import WD_ALIGN_PARAGRAPH
+from pathlib import Path
+from docx import Document
+from docx.enum.text import WD_ALIGN_PARAGRAPH, WD_TAB_ALIGNMENT
 from datetime import datetime
+from docx.shared import Cm, Pt
 
     
 def get_spanish_month(month_number):
@@ -10,28 +13,41 @@ def get_spanish_month(month_number):
     ]
     return months[month_number - 1]
 
-def add_header(doc: Document):
+def agregarEncabezado(doc: Document,numeroDictamen,folio,anio):
     now = datetime.now()
 
     day = now.day
     month = get_spanish_month(now.month)
     year = now.year
 
-    header_text = f"Hermosillo, Sonora, a {day} de {month} de {year}"
+    textoFecha = f"Hermosillo, Sonora, a {day} de {month} de {year}"
 
+    logo_path = Path(__file__).parent / "images" / "logoSTJ.png"
+    stacked_text = f"\tDictamen:{numeroDictamen}/{anio}\n\tReferencia a la solicitud de Soporte Técnico {folio}/{anio}\n\t{textoFecha}"
     for section in doc.sections:
-        header = section.header
-        paragraph = header.paragraphs[0] if header.paragraphs else header.add_paragraph()
+    
+      header = section.header
+      paragraph1 = header.paragraphs[0] if header.paragraphs else header.add_paragraph()
 
-        paragraph.text = header_text
-        paragraph.alignment = WD_ALIGN_PARAGRAPH.RIGHT
+      run = paragraph1.add_run()
+      run.add_picture(str(logo_path), width=Cm(2.5))
+    
+      paragraph2 = header.add_paragraph()
+      paragraph2.alignment = WD_ALIGN_PARAGRAPH.RIGHT
 
-def generarDictamen(unidad,solicitante,elaboro,asignado,fechaRegistro,tipoServicio,fechaAtendido,descripcion,numeroContacto,nombreTitular, puestoTitular):
+      run = paragraph2.add_run(stacked_text)
+      run.bold = True
+      run.font.size = Pt(10)
+      
+def generarDictamen(folio,anio,unidad,solicitante,elaboro,asignado,fechaRegistro,tipoServicio,fechaAtendido,descripcion,numeroContacto,inventario,serie,fechaCompra,nombreTitular, puestoTitular,numeroDictamen):
     #creates file
     doc = Document()
+    #set global style
+    style = doc.styles['Normal']
+    style.font.name = 'Arial'
     #make all changes to file
-    add_header(doc)
+    agregarEncabezado(doc,numeroDictamen,folio,anio)
 
-    doc.add_paragraph("Contenido del documento...")
-    #generates file
+    #doc.add_paragraph("Contenido del documento...")
+    #generates file     
     doc.save("output.docx")
