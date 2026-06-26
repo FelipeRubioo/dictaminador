b8dbf7f3e7e2a067912740319d285ac189b0d5db
FelipeRubioo <feliperubio9201@gmail.com>
2026-06-26 14:03:09 -0700

    filegeneration-introduccion y tabla de informacion del equipo

--- fileGeneration.py
+++ fileGeneration.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from docx import Document
 from docx.enum.text import WD_ALIGN_PARAGRAPH, WD_TAB_ALIGNMENT
 from datetime import datetime
-from docx.shared import Cm, Pt
+from docx.shared import Cm, Pt, RGBColor
 
     
 def get_spanish_month(month_number):
@@ -40,13 +40,39 @@ def agregarEncabezado(doc: Document,numeroDictamen,folio,anio):
       run.font.size = Pt(10)
 
 def agregarTitular(doc: Document,nombreTitular, puestoTitular,unidad):
-    stacked_text = f"\t{nombreTitular}\n\t{puestoTitular}\n\t{unidad}\n\tPresente.-"
+    stacked_text = f"{nombreTitular}\n{puestoTitular}\n{unidad}\nPresente.-"
     paragraph = doc.add_paragraph()
     run = paragraph.add_run(stacked_text)
     run.bold = True
     run.font.size = Pt(12)
 
-def generarDictamen(folio,anio,unidad,solicitante,elaboro,asignado,fechaRegistro,tipoServicio,fechaAtendido,descripcion,numeroContacto,inventario,serie,fechaCompra,nombreTitular, puestoTitular,numeroDictamen):
+def agregarIntroduccion(doc: Document,folio,anio):
+    stacked_text = f"En atención a la solicitud de Soporte Técnico {folio}/{anio}, se emite el presente dictamen."
+    paragraph = doc.add_paragraph()
+    run = paragraph.add_run(stacked_text)
+    run.font.size = Pt(11)
+
+def agregarTabla(doc: Document,solicitante,tipoEquipo,modelo,inventario,serie,fechaCompra):
+    table = doc.add_table(rows=6, cols=2)
+    table.style = 'Table Grid'
+
+    # Fill in the table with data
+    data = [
+        ("Usuario:", solicitante),
+        ("Tipo de Equipo:", tipoEquipo),
+        ("Modelo:", modelo),
+        ("Número de Inventario:", inventario),
+        ("Número de Serie:", serie),
+        ("Fecha de Compra:", fechaCompra)
+    ]
+
+    for row, (label, value) in zip(table.rows, data):
+        row.cells[0].text = label
+        row.cells[1].text = value
+
+        run = row.cells[1].paragraphs[0].runs[0]
+        run.font.color.rgb = RGBColor(0, 0, 255)  # Blue
+def generarDictamen(folio,anio,unidad,solicitante,elaboro,asignado,fechaRegistro,tipoServicio,fechaAtendido,descripcion,numeroContacto,inventario,serie,fechaCompra,nombreTitular, puestoTitular,numeroDictamen,modelo,tipoEquipo):
     #creates file
     doc = Document()
     #set global style
@@ -55,6 +81,8 @@ def generarDictamen(folio,anio,unidad,solicitante,elaboro,asignado,fechaRegistro
     #make all changes to file
     agregarEncabezado(doc,numeroDictamen,folio,anio)
     agregarTitular(doc,nombreTitular,puestoTitular,unidad)
+    agregarIntroduccion(doc,folio,anio)
+    agregarTabla(doc,solicitante,tipoEquipo,modelo,inventario,serie,fechaCompra)
     #doc.add_paragraph("Contenido del documento...")
     #generates file     
     doc.save("output.docx")
